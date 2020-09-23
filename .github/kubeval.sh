e215dae29235e7038ff7ef1c1c64e384fbc2036a
Max <1676208+maxirus@users.noreply.github.com>
2020-09-22 20:13:36 -0400

    Updating kubeval to not error if no chart updates are found

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
-set -euo pipefail
+# set -euo pipefail
+set -e
 
 # https://github.com/instrumenta/kubeval
 
-CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/master -- charts | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
+echo "[INFO] Setting up environment..."
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+CHART_DIRS="$(git diff --find-renames --name-only "${BRANCH}" remotes/origin/master -- charts | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
+if [[ "${CHART_DIRS}" == "" ]]; then
+    echo "[INFO] No chart updates detected"
+    exit 0
+fi
 KUBEVAL_VERSION="0.15.0"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
 
 # install kubeval
+echo "[INFO] Install kubeval..."
 curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
 tar -xf /tmp/kubeval.tar.gz kubeval
+echo "[INFO] kubeval installed"
 
+echo "[INFO] Validating charts.."
 # validate charts
 for CHART_DIR in ${CHART_DIRS}; do
-  helm template --values "${CHART_DIR}"/ci/ci-values.yaml "${CHART_DIR}" | ./kubeval --strict --ignore-missing-schemas --kubernetes-version "${KUBERNETES_VERSION#v}" --schema-location "${SCHEMA_LOCATION}"
+  helm template "${CHART_DIR}" | ./kubeval --strict --ignore-missing-schemas --kubernetes-version "${KUBERNETES_VERSION#v}" --schema-location "${SCHEMA_LOCATION}"
 done
