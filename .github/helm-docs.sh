33e87a7f7d9b51848bef4d7e06c0cf5ad03eae9b
Max <1676208+maxirus@users.noreply.github.com>
2021-09-14 15:53:53 -0400

    Fixing helm-docs action

--- .github/helm-docs.sh
+++ .github/helm-docs.sh
@@ -3,12 +3,12 @@ set -euo pipefail
 
 # https://github.com/norwoodj/helm-docs
 
-HELM_DOCS_VERSION="0.15.0"
+HELM_DOCS_VERSION="1.5.0"
 
 # install helm-docs
 curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz https://github.com/norwoodj/helm-docs/releases/download/v"${HELM_DOCS_VERSION}"/helm-docs_"${HELM_DOCS_VERSION}"_Linux_x86_64.tar.gz
 tar -xf /tmp/helm-docs.tar.gz helm-docs
 
 # validate docs
-./helm-docs
+./helm-docs --chart-search-root=charts/
 git diff --exit-code
